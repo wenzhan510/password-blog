836854aaddfa0eac9ad1216a19d2f2c780a9b8be
Lyn <wenzhan.main@gmail.com>
2018-08-20 00:15:12 -0700

    password

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth')->except(['index','show','password','passwordlogin']);
 
     }
 
